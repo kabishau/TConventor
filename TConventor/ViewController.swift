b9dcddd973f6f5deddd4bad77ce32fa68fa771f2
user131656 <user131656@ny218.local>
2018-02-28 16:32:58 -0500

    UI update and initial cleaning

--- TConventor/ViewController.swift
+++ TConventor/ViewController.swift
@@ -1,24 +1,6 @@
-//
-//  ViewController.swift
-//  TConventor
-//
-//  Created by user131656 on 2/28/18.
-//  Copyright © 2018 user131656. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
