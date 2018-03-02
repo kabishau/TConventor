65323003f30fccfb295293f0314df1cc522e8944
user131656 <user131656@ny218.local>
2018-03-01 19:02:16 -0500

    converter implementation

--- TConventor/ViewController.swift
+++ TConventor/ViewController.swift
@@ -1,6 +1,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var celsiusLabel: UILabel!
+    @IBOutlet weak var fahrenheitLabel: UILabel!
+    @IBOutlet weak var slider: UISlider! {
+        didSet {
+            slider.maximumValue = 100
+            slider.minimumValue = 0
+            slider.value = 0
+        }
+    }
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        
+        let tempCelc = Int(round(sender.value))
+        celsiusLabel.text = "\(tempCelc)ºC"
+        
+        
+        let tempFahr = Int(round((sender.value * 9 / 5) + 32))
+        fahrenheitLabel.text = "\(tempFahr)ºF"
+    }
+    
+    
 
 
 }
